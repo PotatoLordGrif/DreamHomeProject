9e5bfd0341fe8f81ef0adff90b5a5d3ad0e2f339
PotatoLordGrif <32713353+PotatoLordGrif@users.noreply.github.com>
2023-04-17 16:58:54 -0400

    Init

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+ return render_template('home.html')
+
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
+@app.route('/Register')
+def register():
+    return render_template('register.html')
+
+@app.route('/Bookview')
+def bookview():
+    return render_template('bookview.html')
+
+
+
+@app.route('/Rentals')
+def rentals():
+    return render_template('Rentals.html')
+
+@app.route('/addtomail')
+def addtomail():
+    return render_template('addtomail.html')
+
+@app.route('/Property')
+def property():
+    return render_template('property.html')
+
+if __name__ == '__main__':
+    app.run(port=4000,debug=True)
